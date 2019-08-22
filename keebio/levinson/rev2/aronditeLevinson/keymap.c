2757420b971f45889994739c3611dd3fca4a98ef
Joshua Blakley <jblakley89@gmail.com>
2019-08-22 07:08:44 -0700

    Added gaming layer to levinson

--- keebio/levinson/rev2/aronditeLevinson/keymap.c
+++ keebio/levinson/rev2/aronditeLevinson/keymap.c
@@ -10,7 +10,9 @@ enum ergodox_layers {
   _NUM,
   _NAV,
   _FUN,
-  _GAM
+  _GAM,
+  _GAMFUN,
+  _GAMNUM
   };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -23,14 +25,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----+-----+-----+-----+-----+-----|              |-----+-----+-----+-----+-----+-----|
      * | Lsft|  Z  |  X  |  C  |  V  |  B  |              |  K  |  M  |  ,  |  .  |  /? | Rsft|
      * |-----+-----+-----+-----+-----+-----|              |-----+-----+-----+-----+-----+-----|
-     * | Lctr| Lgui| Lalt| _NUM| _NAV| Spc |              | Ent | _SYM| _FUN|     | _MIS| _QWR|
+     * | Lctr| Lgui| Lalt| _NUM| _NAV| Spc |              | Ent | _SYM| _FUN| _MIS| _GAM| _QWR|
      * `-----------------------------------'              `-----------------------------------'
      */
 
     KC_TAB,  KC_Q,   KC_W,   KC_F,   KC_P,   KC_G,       KC_J,   KC_L,   KC_U,   KC_Y,   KC_SCLN,LT(_MIS,KC_ESC),
     KC_BSPC, KC_A,   KC_R,   KC_S,   KC_T,   KC_D,       KC_H,   KC_N,   KC_E,   KC_I,   KC_O,   KC_QUOT,
     KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,       KC_K,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,
-    KC_LCTL,KC_LGUI,KC_LALT,MO(_NUM),MO(_NAV),KC_SPC,    KC_ENT,MO(_SYM),MO(_FUN),KC_NO,MO(_MIS),TG(_QWR)
+    KC_LCTL,KC_LGUI,KC_LALT,MO(_NUM),MO(_NAV),KC_SPC,    KC_ENT,MO(_SYM),MO(_FUN),MO(_MIS),TG(_GAM),TG(_QWR)
     ),
 
 	 /*******Misc  Layer **************************************************************
@@ -136,6 +138,57 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
     KC_TRNS,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
     KC_TRNS,KC_TRNS,KC_TRNS,KC_NO,  KC_NO,  KC_NO,        KC_PSCR,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO
+    ),
+	[_GAM] = LAYOUT(
+    /******* QWERTY Layer **************************************************************
+     * ,-----------------------------------.              ,-----------------------------------.
+     * |     |  Q  |  W  |  E  |  R  |  T  |              |  Y  |  U  |  I  |  O  |  P  | _GAM|
+     * |-----+-----+-----+-----+-----+-----|              |-----+-----+-----+-----+-----+-----|
+     * |     |  A  |  S  |  D  |  F  |  G  |              |  H  |  J  |  K  |  L  |  ;  |  "' |
+     * |-----+-----+-----+-----+-----+-----|              |-----+-----+-----+-----+-----+-----|
+     * |     |  Z  |  X  |  C  |  V  |  B  |              |  N  |  M  |  ,  |  .  |  /? |     |
+     * |-----+-----+-----+-----+-----+-----|              |-----+-----+-----+-----+-----+-----|
+     * |     |     |     |     | _GFN|     |              |     | _GNM|     |     |     |     |
+     * `-----------------------------------'              `-----------------------------------'
+     */
+    KC_TRNS,KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,          KC_Y,  KC_U,   KC_I,   KC_O,   KC_P,   TG(_GAM),
+    KC_TRNS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,          KC_H,  KC_J,   KC_K,   KC_L,   KC_SCLN,KC_TRNS,
+    KC_TRNS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,          KC_N,  KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,
+    KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,MO(_GAMFUN),KC_TRNS,KC_TRNS,  MO(_GAMNUM),KC_TRNS,KC_RALT,KC_RGUI,KC_RCTL
+    ),
+	[_GAMFUN] = LAYOUT(
+    /******* FUNCTION Layer **************************************************************
+     * ,-----------------------------------.              ,-----------------------------------.
+     * |     | F1  | F2  | F3  | F4  | F5  |              | F6  | F7  | F8  |  F9 | F10 | F11 |
+     * |-----+-----+-----+-----+-----+-----|              |-----+-----+-----+-----+-----+-----|
+     * |     |  1  |  2  |  3  |  4  |  5  |              |     |     |     |     |     | F12 |
+     * |-----+-----+-----+-----+-----+-----|              |-----+-----+-----+-----+-----+-----|
+     * |     |     |     |     |     |     |              |     |     |     |     |     |     |
+     * |-----+-----+-----+-----+-----+-----|              |-----+-----+-----+-----+-----+-----|
+     * |     |     |     |     |     |     |              |     |     |     |     |     |     |
+     * `-----------------------------------'              `-----------------------------------'
+     */
+    KC_NO,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,        KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,
+    KC_NO,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,         KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_F12,
+    KC_TRNS,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+    KC_TRNS,KC_TRNS,KC_TRNS,KC_NO,  KC_TRNS,KC_NO,        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO
+    ),
+	[_GAMNUM] = LAYOUT(
+    /******* SYMBOL Layer **************************************************************
+     * ,-----------------------------------.              ,-----------------------------------.
+     * |     |     |     |     |     |     |              | NLCK|  7  |  8  |  9  |  *  |  /  |
+     * |-----+-----+-----+-----+-----+-----|              |-----+-----+-----+-----+-----+-----|
+     * |     |     |     |     |     |     |              |     |  4  |  5  |  6  |  +  |  -  |
+     * |-----+-----+-----+-----+-----+-----|              |-----+-----+-----+-----+-----+-----|
+     * |     |     |     |     |     |     |              |     |  1  |  2  |  3  |  =  | Rsft|
+     * |-----+-----+-----+-----+-----+-----|              |-----+-----+-----+-----+-----+-----|
+     * |     |     |     |     |     |     |              | Ent |     |  0  |  .  |     |     |
+     * `-----------------------------------'              `-----------------------------------'
+     */
+     KC_TRNS,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,     KC_NLCK,KC_P7,  KC_P8,  KC_P9,  KC_PAST,KC_PSLS,
+     KC_TRNS,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,     KC_NO,  KC_P4,  KC_P5,  KC_P6,  KC_PPLS,KC_PMNS,
+     KC_TRNS,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,     KC_NO,  KC_P1,  KC_P2,  KC_P3,  KC_EQL, KC_RSFT,
+     KC_TRNS,KC_TRNS,KC_TRNS,KC_NO,  KC_NO,  KC_TRNS,   KC_TRNS,KC_TRNS,KC_P0,  KC_DOT, KC_NO,  KC_NO
     )
 };
 
